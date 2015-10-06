41ba7a1b7ba75bda76307ef0b8d65bcecd4be833
Jie Yang <jieyang@jiedemacbook-pro.local>
2015-10-06 17:55:34 +0800

    Initial Commit

---
+++ TestIosProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestIosProject
+//
+//  Created by Jie Yang on 15/10/6.
+//  Copyright © 2015年 Jie Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestIosProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestIosProject
+//
+//  Created by Jie Yang on 15/10/6.
+//  Copyright © 2015年 Jie Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
